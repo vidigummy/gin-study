f1ea811535b5fd73452bd19f82fc2f1ff676a184
Ryu Dong-In <33488236+vidigummy@users.noreply.github.com>
2022-12-05 20:30:16 +0900

    create user 성공

--- models/User.go
+++ models/User.go
@@ -7,19 +7,19 @@ import (
 
 type User struct {
 	gorm.Model
-	idx       int64  `json:"idx" gorm:"primary_key"`
-	user_name string `json:"user_name" gorm:"-:all", "column:user_name"`
-	email     string `json:"email" gorm:"-:all", "column:email"`
+	Idx      int64 `json:"idx" gorm:"primary_key"`
+	UserName string
+	Email    string
 }
 
 func SetUser(username string, email string) *User {
-	newUser := User{user_name: username, email: email}
+	newUser := User{UserName: username, Email: email}
 	return &newUser
 }
 
 func CommitUser(u *User) error {
 	db := database.Database()
-	result := db.Create(&User{user_name: u.user_name, email: u.email})
+	result := db.Create(&User{UserName: u.UserName, Email: u.Email})
 	if result.Error != nil {
 		return result.Error
 	}
