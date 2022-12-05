82b07be19053cb708b81b19262c086fa180d09cd
Ryu Dong-In <33488236+vidigummy@users.noreply.github.com>
2022-12-06 00:24:58 +0900

    main은 가벼워야 한다.

--- main.go
+++ main.go
@@ -2,19 +2,7 @@ package main
 
 import (
 	"github.com/dev-yakuza/study-golang/gin/start/router"
-	"github.com/gin-gonic/gin"
 )
-
-func setupRouter() *gin.Engine {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.String(200, "pong")
-	})
-	r.GET("/", func(c *gin.Context) {
-		c.String(200, "ping")
-	})
-	return r
-}
 
 func main() {
 	r := router.SetupRouter()
